273f6c81010c88529f7ee424443c466568ba348a
realbewhy <realbewhy@gmail.com>
2018-10-26 10:52:09 +0700

    api

---
+++ api/api/controllers/UsersController.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const util = require('util')
+const mysql = require('mysql')
+const db = require('./../db')
+
+module.exports = {
+    get: (req, res) => {
+        let sql = 'SELECT * FROM info'
+        db.query(sql, (err, response) => {
+            if (err) throw err
+            res.json(response)
+        })
+    },
+    store: (req, res) => {
+        let data = req.body;
+        let sql = 'INSERT INTO info SET ?';
+        db.query(sql, [data], (err, response) => {
+            if (err) throw err
+            res.json({ message: 'Insert success!' })
+        });
+    },
+    delete: (req, res) => {
+        console.log('abc')
+        let sql = 'DELETE FROM info WHERE id = ?'
+        db.query(sql, [req.params.id], (err, response) => {
+            if (err) throw err
+            res.json({message: 'Delete success!'})
+        })
+    }
+}
---
+++ api/api/routes.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = function (app) {
+  let productsCtrl = require('./controllers/UsersController');
+
+  // app.use(function (req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
+
+  app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  });
+
+
+  // todoList Routes
+  app.route('/api/users')
+    .get(productsCtrl.get)
+    .post(productsCtrl.store);
+
+  app.route('/api/users/:id')
+    .delete(productsCtrl.delete);
+};
